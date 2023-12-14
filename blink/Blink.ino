486ecfd4f78e77068bc6a0bf13835df7de1e0eec
gzehentner <gzehentner@web.de>
2023-12-14 18:53:07 +0100

    modified:   blink/Blink.ino 	new file:   libraries/readme.txt

--- blink/Blink.ino
+++ blink/Blink.ino
@@ -21,7 +21,7 @@
 
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
 */
-#define flash 4
+#define flash 2
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
